6cbf7d43423eb6178d56be210dd19b557ffe411c
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2020-05-29 23:15:03 +0600

    fixing the nav-bar

--- frontend/app/src/components/Home/Cards/cards.js
+++ frontend/app/src/components/Home/Cards/cards.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./cards.css";
 
-const card=(props)=>{
-    const imgid="props.imgid";
-    return(
-        <div className="container">
-           <div className="card">
-                    <img className="card-img-top" src={props.imgid} alt="Card image cap" width='200px' height="200px"/>
-                    <div className="card-body">
-                        <h5 className="card-title">{props.name}</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        {/* <a href="#" className="btn btn-primary">Learn More</a> */}
-                    </div>
-                </div>            
+const card = (props) => {
+  const imgid = "props.imgid";
+  return (
+    <div className="container">
+      <div className="card">
+        <img
+          className="card-img-top"
+          src={props.imgid}
+          alt="Card image cap"
+          width="200px"
+          height="200px"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{props.name}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          {/* <a href="#" className="btn btn-primary">Learn More</a> */}
         </div>
-    );
-
-}
+      </div>
+    </div>
+  );
+};
 
 export default card;
