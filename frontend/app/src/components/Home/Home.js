e5c2ef62c19a06422f0958829709f9197b558b44
Jawad-haider-UCA <46367771+Jawad-haider-UCA@users.noreply.github.com>
2020-05-31 17:02:41 +0600

    Add files via upload

--- frontend/app/src/components/Home/Home.js
+++ frontend/app/src/components/Home/Home.js
@@ -5,11 +5,11 @@ import pic1 from './1.PNG';
 import pic2 from './img1.png';
 import pic3 from "./pic1.JPG";
 import logo1 from './elcart.png';
-import logo2 from "./MP.png";
+import logo2 from "./p2.png";
+import logo3 from "./p3.png";
 import Cards from "../Home/Cards/cards";
 import logos from "./Logoss.png";
 import HowIttWorks from "./HowItWorks/Works";
-import debit from './debit.jpeg';
 
 
 export default class Home extends Component{
@@ -18,19 +18,18 @@ export default class Home extends Component{
         return( 
           <React.Fragment>
             {/* The Side sticky icons */}
-            <div class="sticky_icons d-none d-sm-block" style={{ backgroundColor:'#f7f7f7'}}>
+            <div class="sticky_icons d-none d-sm-block">
               <ul class="list-unstyled">
-                  <li style={{fontSize:'15px', fontWeight:'bold'}}>Pay via</li>
-                  <br/>
+                  <li>Pay via</li>
                   <li class="pb-4">
                       <img width="50" alt="elcart logo" src={logo1}/>
                   </li>
                   <li class="pb-4">
-                      <img width="60" alt="MegaPay logo" src={logo2}/>
+                      <img width="50" alt="MegaPay logo" src={logo2}/>
 
                   </li>
                   <li>
-                      <img width="60" alt="logo" src={debit}/>
+                      <img width="50" alt="logo" src={logo3}/>
                   </li>
               </ul>
             </div>
@@ -38,32 +37,31 @@ export default class Home extends Component{
 
 
             {/* -------------The Main Banner ----------------------*/}
-            <br/>
             <section class="banner-section mb-sm-5">
 
               <div class="container">
                   <div class="row">
-                      <div class="col-lg-6 col-md-8 my-auto" style={{ paddingLeft:'4%'}}>
+                      <div class="col-lg-6 col-md-8 my-auto">
                           <h1 class="title">Drive Ur <br/>Mind Bravely</h1>
 
                           <h3 class="text-primary font-nm mb-2 mb-sm-3">
                               Solve Problems &amp; build your Mind,<br></br> Work out &amp; Build Your Body
                           </h3>
-                          <img width="400" class="img-fluid board-logos mb-3 imglogos" alt="Logos to be made and put" src={logos} style={{marginLeft:'-2%'}}/>
+                          <img width="400" class="img-fluid board-logos mb-3 imglogos" alt="Logos to be made and put" src={logos}/>
                           <div>
-                              <a class="btn btn-banner d-none d-sm-inline-block" href="#pointer">learn more</a>
+                              <a class="btn btn-banner d-none d-sm-inline-block" href="#">learn more</a>
+                              <a class="btn btn-banner d-inline-block d-sm-none" href="#">free trial</a>
                           </div>
                       </div>
-                      <div class="col-lg-6 col-md-4 col-3 d-none d-sm-block"style={{ paddingRight:'7%'}}>
-                          <img class="img-fluid imgBanner" alt="Banner Image" src={pic1} style={{height:'410px',  width:'1200px'}}/>
+                      <div class="col-lg-6 col-md-4 col-3 my-auto d-none d-sm-block">
+                          <img class="img-fluid imgBanner" alt="Banner Image" src={pic1}/>
                       </div>
                   </div>
               </div>
           </section>
-          <br/>
           {/*------------ End of Banner Section------- */}
           {/* -----------The Cards------------------------ */}
-          <div className="container" style={{ marginTop:'5%', marginBottom:'10%'}}>
+          <div className="container">
               <div className="row">
                   <div className="col-md-4">
                       <Cards name="Body" imgid={pic1}/>
@@ -81,7 +79,6 @@ export default class Home extends Component{
           {/*-------------------- The End Three Cards---------------- */}
           {/* ---------------How it works Portion-------------------- */}
           <HowIttWorks />
-          <br/>
           
           {/* ---------------How it works Portion-------------------- */}
 
--- frontend/app/src/components/Home/HowItWorks/Works.js
+++ frontend/app/src/components/Home/HowItWorks/Works.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import "./Wroks.css";
-
+import dots from "./Dots.PNG";
+import icon1 from "./icon1.png";
+import icon2 from "./icon2.png";
+import icon3 from "./icon3.png";
+import d1 from "./d1.png";
+import d3 from "./d2.png";
+import d2 from "./d3.png";
+import d4 from "./d4.png";
+import s1 from "./s1.jpg";
+import s2 from "./s2.jpg";
+import s3 from "./s3.jpg";
 
 class Works extends Component {
     render() {
         return (
             <div>
                 {/* How it works Portion */}
-                <section class="section_gap how-it-works-section" id="how-it-works" style={{marginBottom:'5%'}}>                 
+                <section class="section_gap how-it-works-section" id="how-it-works">                 
                     <div class="container">                     
                         <div class="row">
                             <div class="col-md-6 mb-4 mb-sm-auto">
@@ -24,7 +35,7 @@ class Works extends Component {
                                         <div class="mb-md-3">
                                             <div class="d-flex align-items-center">
                                                 <div>
-                                                    <img src="" alt="Icon" width="40" class="mr-3"/>
+                                                    <img src={icon1} alt="Icon" width="40" class="mr-3"/>
                                                 </div>
                                                 <h5 class="mb-0 text-blue">Create your account</h5>
                                             </div>
@@ -32,7 +43,7 @@ class Works extends Component {
                                         <div class="mb-md-3">
                                             <div class="d-flex align-items-center">
                                                 <div>
-                                                    <img src="" alt="Icon" width="40" class="mr-3"/>
+                                                    <img src={icon2} alt="Icon" width="40" class="mr-3"/>
                                                 </div>
                                                 <h5 class="mb-0 text-blue">Select your Courses</h5>
                                             </div>
@@ -40,7 +51,7 @@ class Works extends Component {
                                         <div class="mb-md-3">
                                             <div class="d-flex align-items-center">
                                                 <div>
-                                                    <img src="" alt="Icon" width="40" class="mr-3"/>
+                                                    <img src={icon3} alt="Icon" width="40" class="mr-3"/>
                                                 </div>
                                                 <h5 class="mb-0 text-blue">Start your free trail</h5>
                                             </div>
@@ -53,8 +64,7 @@ class Works extends Component {
                 </section>
                  {/* -------------X-------How it works Portion ---------X---------------*/}
                 {/* -----------------------Information- Statistics ------------------------*/}
-                <hr/>
-                <section class="section_gap d-none d-sm-block" id="pointer">
+                <section class="section_gap d-none d-sm-block">
                     <div class="container">
                         <div class="row text-center" id="counter">
                             <div class="col-md-4">
@@ -72,29 +82,27 @@ class Works extends Component {
                         </div>
                     </div>
                 </section>
-                <hr/>
                 {/* ----------X-------------Information- Statistics ----------X--------------*/}
                 {/*----------------------------- Why Chose DUMB Section ---------------------------- */}
-
-                <section class="section_gap2"  style={{marginTop:'5%', marginBottom:'2%'}}>
+                <section class="section_gap2">
                     <div class="container">
                         <div class="row ">
                             <div class="col-md-12 ">
                                 <h2 class="title"><span class="text-purple">why choose </span>
-                                    <span class="text-blue">DUMB?</span>
+                                    <span class="text-blue">DUMB</span>
                                 </h2>
                                 <p class="h5 mt-4">
-                                    DUMB is not just an e-learning platform, it is the gateway towards securing a
+                                    tutoria is not just an e-learning platform, it is the gateway towards securing a
                                     brighter future for your academic and professional life.
                                 </p>
                             </div>
                         </div>
 
-                        <div class="row mt-4 feature-section" >
+                        <div class="row mt-4 feature-section">
                             <div class="col-md-6">
                                 <div class="d-flex justify-content-between mb-3">
                                     <div class="mr-sm-5 mr-3">
-                                        <img src="https://tutoria.pk/public/assets/assets-site/images/icon1.svg" class="img-fluid"/>
+                                        <img src={d1} class="img-fluid"/>
                                     </div>
                                     <div>
                                         <h4 class="card-title">learn with any device</h4>
@@ -112,7 +120,7 @@ class Works extends Component {
                             <div class="col-md-6">
                                 <div class="d-flex justify-content-between mb-3">
                                     <div class="mr-sm-5 mr-3">
-                                        <img src="https://tutoria.pk/public/assets/assets-site/images/fast_support.svg" class="img-fluid"/>
+                                        <img src={d2} class="img-fluid"/>
                                     </div>
                                     <div>
                                         <h4 class="card-title">fastest support</h4>
@@ -129,7 +137,7 @@ class Works extends Component {
                             <div class="col-md-6">
                                 <div class="d-flex justify-content-between mb-3">
                                     <div class="mr-sm-5 mr-3">
-                                        <img src="https://tutoria.pk/public/assets/assets-site/images/own_time.svg" class="img-fluid"/>
+                                        <img src={d3} class="img-fluid"/>
                                     </div>
                                     <div>
                                         <h4 class="card-title">study at your own time</h4>
@@ -147,7 +155,7 @@ class Works extends Component {
                             <div class="col-md-6">
                                 <div class="d-flex justify-content-between mb-3">
                                     <div class="mr-sm-5 mr-3">
-                                        <img src="https://tutoria.pk/public/assets/assets-site/images/certified_teachers.svg" class="img-fluid"/>
+                                        <img src={d4} class="img-fluid"/>
                                     </div>
                                     <div>
                                         <h4 class="card-title">certified teachers</h4>
@@ -168,9 +176,114 @@ class Works extends Component {
                 </section>
 
                 {/*-----------------X------------ Why Chose DUMB Section --------X-------------------- */}
-                {/* -----------------Words from Mentors--------------------------------- */}
+                {/* -----------------Get Assistance--------------------------------- */}
+                <section class="section_gap3">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-4 my-auto">
+                                <img class="img-fluid" src= {dots}alt=""/>
+                            </div>
+                            <div class="col-lg-6 col-md-8 my-auto order-sm-first">
+                                <h2 class="title2">
+                                    <span class="text-blue">get</span>
+                                    <span class="text-pink">free</span><br/>
+                                    <span class="text-blue">assistance from our expert teachers</span>
+                                </h2>
+                                <div class="mt-sm-4">
+                                    <a href="/#" class="btn btn-outline-pink">
+                                        +996 -777 438 662
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </section>                
+                {/* ---------X--------Get Assistan-----------X---------------------- */}
+                {/* -----------------Words from  Mentors--------------------------------- */}
+            {/* <div className="containerr">
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={s1}
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={s2}
+                        alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={s3}
+                        alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div> */}
                 
                 {/* ---------X--------Words from Mentors-----------X---------------------- */}
+                {/* -----------------Blogs--------------------------------- */}
+                <section class="section_gap4">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-5 my-auto">
+                                <h2 class="title"><span class="text-purple">sign up </span><span class="text-pink">now!</span></h2>
+                                <p class="h5 my-4">
+                                    ready to get started? simply fill out this form to register for
+                                    our exclusive seven-day free trial!
+                                </p>
+                            </div>
+                            <div class="col-lg-6 col-md-7 my-auto">
+                                <div class="card shadow border-0">
+                                    <form method="POST" action="#" id="user_signupForm">
+                                        <div class="card-body p-lg-5 p-4">
+                                            <h5 class="card-title h3 text-blue mb-4">free trial</h5>
+                                            <div class="card-text register">                                        
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control bottom_bar" id="first_name" name="first_name" placeholder="First Name" required=""/>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control bottom_bar" id="user_mobile" name="user_mobile" minlength="11" maxlength="11" onkeypress="return (event.charCode == 8 || event.charCode == 0 || event.charCode == 13) ? null : event.charCode >= 48 &amp;&amp; event.charCode <= 57" placeholder="Phone Number" required=""/>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="email" class="form-control bottom_bar" id="user_email" name="user_email" placeholder="Email" required=""/>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="password" class="form-control bottom_bar" id="password" name="password" minlength="6" maxlength="12" placeholder="Password" required=""/>
+                                                </div>
+                                            </div>
+                                            <div class="text-center">
+                                                <button type="submit" class="btn btn-pink next">register</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>                
+                {/* ---------X--------Blogs-----------X---------------------- */}
+
             </div>
         )
     }
